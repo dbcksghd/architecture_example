c2a1aecdde04efd613b58135e9289b65d52a2a58
yoochanhong <chanhongy6@naver.com>
2023-08-04 18:32:33 +0900

    Bloc 주입

--- bloc_example/lib/main.dart
+++ bloc_example/lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:bloc_example/application/bloc/todo_bloc.dart';
+import 'package:bloc_example/application/presentation/todo_page.dart';
+import 'package:bloc_example/data/data_soruce/todo_data_source.dart';
+import 'package:bloc_example/data/repository/todo_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,23 +12,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: RepositoryProvider(
+        create: (context) => TodoRepository(TodoDataSource()),
+        child: BlocProvider(
+          create: (context) => TodoBloc(context.read<TodoRepository>()),
+          child: const TodoPage(),
+        ),
+      ),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
   }
 }
